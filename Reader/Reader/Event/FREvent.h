e910073b2b4c4e3d3004ebb1c7e511c2797a3a24
qianjianeng <329426491@qq.com>
2017-01-24 18:17:39 +0800

    添加事件统计SDk

---
+++ Reader/Reader/Event/FREvent.h
@@ -0,0 +1,13 @@
+//
+//  FREvent.h
+//  Reader
+//
+//  Created by qianjn on 2017/1/24.
+//  Copyright © 2017年 SF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FREvent : NSObject
+
+@end
---
+++ Reader/Reader/Event/FREvent.m
@@ -0,0 +1,13 @@
+//
+//  FREvent.m
+//  Reader
+//
+//  Created by qianjn on 2017/1/24.
+//  Copyright © 2017年 SF. All rights reserved.
+//
+
+#import "FREvent.h"
+
+@implementation FREvent
+
+@end
