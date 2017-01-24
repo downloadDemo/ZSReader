11d61c58636368b1e02e32854f38c909f6661abc
qianjianeng <329426491@qq.com>
2017-01-24 15:54:18 +0800

    init

---
+++ Reader/Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reader
+//
+//  Created by qianjn on 2017/1/24.
+//  Copyright © 2017年 SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
